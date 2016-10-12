650edbd30ddc50195e424aec1439455ed5401566
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-10-12 18:13:34 +0300

    Refs #74985 Bise; tweaks and changes to tiles

--- bise/ecosystemservices/tiles/contents.py
+++ bise/ecosystemservices/tiles/contents.py
@@ -6,8 +6,10 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from collective.cover.tiles.base import IPersistentCoverTile
 from collective.cover.tiles.base import PersistentCoverTile
 from plone.app.uuid.utils import uuidToObject
+from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.tiles.interfaces import ITileDataManager
 from plone.uuid.interfaces import IUUID
+from z3c.relationfield.schema import RelationChoice
 from zope.interface import implements
 from zope.schema import TextLine
 import logging
@@ -22,10 +24,10 @@ class IFolderContentsListingTile(IPersistentCoverTile):
         required=False,
     )
 
-    uuid = TextLine(
-        title=u'UUID',
+    linked_folder = RelationChoice(
+        title=u"Folder Root",
+        source=ObjPathSourceBinder(),
         required=False,
-        readonly=True,
     )
 
 
@@ -39,34 +41,35 @@ class FolderContentsListingTile(PersistentCoverTile):
 
     is_configurable = False
     is_editable = True
-    is_droppable = True
+    is_droppable = False
     short_name = u'Folder Contents'
 
     def children(self):
-        obj = uuidToObject(self.data['uuid'])
+        obj = self.data.get('linked_folder')
         if not obj:
             return []
+        import pdb; pdb.set_trace()
         return obj.getFolderContents()
 
     def title(self):
         return self.data.get('title', 'Missing tile title')
 
     def is_empty(self):
-        return not (self.data.get('uuid', None))
+        return not (self.data.get('linked_folder', None))
 
     def accepted_ct(self):
         """Return an empty list as no content types are accepted."""
         return ['Folder', 'FolderishPage']
 
-    def populate_with_object(self, obj):
-        super(FolderContentsListingTile, self).populate_with_object(obj)
-
-        if obj.portal_type not in self.accepted_ct():
-            return
-
-        data = {
-            'title': safe_unicode(obj.Title()),
-            'uuid': IUUID(obj),
-        }
-        data_mgr = ITileDataManager(self)
-        data_mgr.set(data)
+    # def populate_with_object(self, obj):
+    #     super(FolderContentsListingTile, self).populate_with_object(obj)
+    #
+    #     if obj.portal_type not in self.accepted_ct():
+    #         return
+    #
+    #     data = {
+    #         'title': safe_unicode(obj.Title()),
+    #         'uuid': IUUID(obj),
+    #     }
+    #     data_mgr = ITileDataManager(self)
+    #     data_mgr.set(data)
