8df446b6df90fac188d804771716f51306611226
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-04-11 17:03:49 +0300

    Refs #83024 Wise; remove moved content

--- bise/ecosystemservices/vocabulary.py
+++ bise/ecosystemservices/vocabulary.py
@@ -1,38 +1,10 @@
-from Products.CMFCore.utils import getToolByName
 from plone.memoize import ram
 from time import time
-from zope.interface import alsoProvides, implements
+from zope.interface import implements
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-from zope.site.hooks import getSite
 import json
 import requests
-
-
-def datasource_vocabulary_factory(ptype):
-    def factory(context):
-        try:
-            catalog = getToolByName(context, 'portal_catalog')
-        except AttributeError:
-            catalog = getToolByName(getSite(), 'portal_catalog')
-
-        brains = catalog(portal_type=ptype)
-        terms = [SimpleTerm(b.UID, b.UID, b.Title) for b in brains]
-
-        return SimpleVocabulary(terms)
-
-    return factory
-
-
-sparql_vocabulary = datasource_vocabulary_factory("Sparql")
-alsoProvides(sparql_vocabulary, IVocabularyFactory)
-
-elasticsearch_vocabulary = datasource_vocabulary_factory('ElasticSearch')
-alsoProvides(elasticsearch_vocabulary, IVocabularyFactory)
-
-datasource_vocabulary = datasource_vocabulary_factory(['Sparql',
-                                                       'ElasticSearch'])
-alsoProvides(datasource_vocabulary, IVocabularyFactory)
 
 
 def _cache_key(fun, *args):
